900a464a14d8d6b788a6d8266650a24e8fac0894
Bahhou <mohsinebahhou@gmail.com>
2020-03-29 01:55:54 +0100

    WIP Add Product / Add picture with camera phone

--- app/src/main/java/fr/uge/projetandroid/AjouterProduit.java
+++ app/src/main/java/fr/uge/projetandroid/AjouterProduit.java
@@ -1,7 +1,17 @@
 package fr.uge.projetandroid;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +21,27 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import fr.uge.projetandroid.entities.Product;
 
@@ -31,10 +54,21 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
     private EditText editTextNom;
     private EditText editTextPrix;
     private EditText editTextDescrition;
-   // private Button buttonUploadImage;
     private ImageButton buttonUploadImage;
     private Button buttonAjouter;
     private ProgressDialog pDialog;
+    private ImageView imageView;
+    public static final int REQUEST_IMAGE = 100;
+    public static final int REQUEST_PERMISSION = 200;
+    private String imageFilePath = "";
+    int serverResponseCode = 0;
+    /************* Php script path ****************/
+    String upLoadServerUri = "http://makcenter.ma/uge/projetAndroid/uploadimage.php";
+    /**********  File Path *************/
+    String uploadFilePath ;
+    String uploadFileName ;
+    int ResponseCodePhpServer=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +85,7 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
         editTextDescrition = (EditText)  findViewById(R.id.editText_ajouterProduit_description);
         buttonUploadImage = (ImageButton)  findViewById(R.id.button_ajouterProduit_photo);
         buttonAjouter = (Button)  findViewById(R.id.button_ajouterproduit_ajouter);
+        imageView = findViewById(R.id.image);
 
 
         ArrayAdapter<CharSequence> adapterEtat = ArrayAdapter.createFromResource(this,
@@ -122,7 +157,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_bibliotheque_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                   // spinnerType.setOnItemSelectedListener(AjouterProduit.this);
 
                 }
                 else if(categrorie.equals("Electronique")){
@@ -130,7 +164,7 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_electronique_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                   // spinnerType.setOnItemSelectedListener(AjouterProduit.this);
+
 
                 }
                 else if(categrorie.equals("Mode et vetements")){
@@ -138,7 +172,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_modevetement_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                    //spinnerType.setOnItemSelectedListener(AjouterProduit.this);
 
                 }
                 else if(categrorie.equals("Musique")){
@@ -146,7 +179,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_music_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                    //spinnerType.setOnItemSelectedListener(AjouterProduit.this);
 
                 }
                 else if(categrorie.equals("Accessoires")){
@@ -154,7 +186,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_accessoire_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                    //spinnerType.setOnItemSelectedListener(AjouterProduit.this);
 
                 }
                 else if(categrorie.equals("Autre")){
@@ -162,8 +193,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                             R.array.type_autre_array, android.R.layout.simple_spinner_item);
                     adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerType.setAdapter(adapterType);
-                   // spinnerType.setOnItemSelectedListener(AjouterProduit.this);
-
                 }
             }
 
@@ -181,11 +210,82 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
                 product.setPrice(Double.parseDouble(editTextPrix.getText().toString()));
                 product.setDescription(editTextDescrition.getText().toString());
                 new AjouterProduit.AddProductTask().execute();
-
             }
         });
 
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        buttonUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCameraIntent();
+            }
+        });
+
+
+    }
+
+
+    private void openCameraIntent() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            Uri photoUri = FileProvider.getUriForFile(this, getPackageName() +".provider", photoFile);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(pictureIntent, REQUEST_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Thanks for granting Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(Uri.parse(imageFilePath));
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "You cancelled the operation", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException{
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        imageFilePath = image.getAbsolutePath();
+        uploadFileName=imageFileName;
+        uploadFilePath=imageFilePath;
+
+        return image;
     }
 
 
@@ -196,7 +296,6 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-
 
 
     private class AddProductTask extends AsyncTask<Void, Void, Void> {
@@ -213,29 +312,155 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
 
         @Override
         protected Void doInBackground(Void... arg0) {
-            String urlString = "http://uge-webservice.herokuapp.com/api/product/";
-            OutputStream out = null;
 
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setRequestProperty("Content-Type","application/json");
-                out = new BufferedOutputStream(urlConnection.getOutputStream());
+            String fileName = uploadFilePath;
 
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
-                writer.write(product.toJson());
-                writer.flush();
-                writer.close();
-                out.close();
-                Log.e("produit1",product.toJson());
-                urlConnection.connect();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                Log.e("Erreur",e.getMessage());
+            HttpURLConnection conn = null;
+            DataOutputStream dos = null;
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            int bytesRead, bytesAvailable, bufferSize;
+            byte[] buffer;
+            int maxBufferSize = 1 * 1024 * 1024;
+            File sourceFile = new File(uploadFilePath);
+
+            if (!sourceFile.isFile()) {
+                Log.e("uploadFile", "Source File not exist :"
+                        +uploadFilePath + "" + uploadFileName);
+
+            }
+            else {
+                Log.e("path", uploadFilePath);
+                try {
+                    Log.e("mohsine2", "mohsine2");
+                    // open a URL connection to the Servlet
+                    FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                    URL url = new URL(upLoadServerUri);
+
+                    // Open a HTTP  connection to  the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // Allow Inputs
+                    conn.setDoOutput(true); // Allow Outputs
+                    conn.setUseCaches(false); // Don't use a Cached Copy
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                    conn.setRequestProperty("uploaded_file", fileName);
+
+                    dos = new DataOutputStream(conn.getOutputStream());
+
+                    dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                            + fileName + "\"" + lineEnd);
+
+                    dos.writeBytes(lineEnd);
+
+                    // create a buffer of  maximum size
+                    bytesAvailable = fileInputStream.available();
+
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // read file and write it into form...
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    }
+
+                    // send multipart form data necesssary after file data...
+                    dos.writeBytes(lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                    // Responses from the server (code and message)
+                    serverResponseCode = conn.getResponseCode();
+                    ResponseCodePhpServer=serverResponseCode;
+                    String serverResponseMessage = conn.getResponseMessage();
+
+                    Log.e("uploadFile", "HTTP Response is : "
+                            + serverResponseMessage + ": " + serverResponseCode);
+
+
+                    //close the streams //
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+
+                    if (serverResponseCode == 200) {
+
+                        HttpURLConnection urlConnection;
+                        String url2 = "http://uge-webservice.herokuapp.com/api/product/";
+                        String data = product.toJson();
+                        String result = null;
+                        try {
+                            //Connect
+                            urlConnection = (HttpURLConnection) ((new URL(url2).openConnection()));
+                            urlConnection.setDoOutput(true);
+                            urlConnection.setRequestProperty("Content-Type", "application/json");
+                            urlConnection.setRequestProperty("Accept", "application/json");
+                            urlConnection.setRequestMethod("POST");
+                            urlConnection.connect();
+
+                            //Write
+                            OutputStream outputStream = urlConnection.getOutputStream();
+                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                            writer.write(data);
+                            writer.close();
+                            outputStream.close();
+
+                            //Read
+                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+                            String line = null;
+                            StringBuilder sb = new StringBuilder();
+
+                            while ((line = bufferedReader.readLine()) != null) {
+                                sb.append(line);
+                            }
+
+                            bufferedReader.close();
+                            result = sb.toString();
+                            Log.e("Json", data);
+                            Log.e("Message retour", "resultat : " + result);
+
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                            Log.e("Erreur", e.getMessage());
+                        }
+                        //return result;
+                    }
+
+                } catch (MalformedURLException ex) {
+
+
+                    ex.printStackTrace();
+
+
+                    Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+                } catch (Exception e) {
+
+
+                    e.printStackTrace();
+
+
+                    Log.e("Upload file error", "Exception : "
+                            + e.getMessage(), e);
+                }
+
             }
             return null;
         }
+
+
+
 
         @Override
         protected void onPostExecute(Void result) {
@@ -245,6 +470,12 @@ public class AjouterProduit extends AppCompatActivity implements AdapterView.OnI
             if (pDialog.isShowing())
                 pDialog.dismiss();
 
+            if(ResponseCodePhpServer==200){
+                Toast.makeText(AjouterProduit.this, "Produit ajouté avec succes", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(AjouterProduit.this, "Erreur de connexion veuillez reessayer", Toast.LENGTH_SHORT).show();
+            }
 
             Log.e("produit2",product.toJson());
 
