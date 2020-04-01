5bd03a24eecaba38aa93883f967e06692356dcf8
Bahhou <mohsinebahhou@gmail.com>
2020-04-01 06:34:40 +0200

    WIP Show Product / Time and date picker

--- app/src/main/java/fr/uge/projetandroid/AfficherProduitEmprunt.java
+++ app/src/main/java/fr/uge/projetandroid/AfficherProduitEmprunt.java
@@ -1,20 +1,32 @@
 package fr.uge.projetandroid;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Handler;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-public class AfficherProduitEmprunt extends AppCompatActivity {
+import fr.uge.projetandroid.entities.Product;
+
+public class AfficherProduitEmprunt extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private ImageView Imageproduit;
 
@@ -30,6 +42,23 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
     private TextView textView_seconde;
 
 
+    private Button emprunter;
+    private Button demandeEmprunt;
+    private Button nouveauEmprunt;
+    private Button nouvelleDemandeEmprunt;
+    private Button button_dateDebut_emprunt;
+    private Button button_heureDebut_emprunt;
+    private Button button_dateFin_emprunt;
+    private Button button_heureFin_emprunt;
+
+
+    private LinearLayout layout_boutton_emprunt;
+    private LinearLayout layout_saisirDate_emprunt;
+    private LinearLayout layout_demande_emprunt_boutton;
+    private LinearLayout layout_saisirDate_DemandeEmprunt;
+    private LinearLayout layout_demande_emprunt_countdown;
+
+
     private String EVENT_DATE_TIME = "2020-03-31 05:02:00";
     private String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private LinearLayout linear_layout_jour, linear_layout_heure;
@@ -37,10 +66,34 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
     private Runnable runnable;
 
 
+    private int note=0;
+
+    private Product product;
+    private String date,heure;
+
+    private DialogFragment datePickerDebutEmprunt;
+    private DialogFragment datePickerFinEmprunt;
+    private DialogFragment timePickerDebutEmprunt;
+    private DialogFragment timePickerFinEmprunt;
+
+    public static final int TYPE_DIALOG_FRAGMENT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afficher_produit_emprunt);
+
+        product = new Product();
+        product.setAvailable(true);
+
+        emprunter=(Button)findViewById(R.id.button_emprunter);
+        demandeEmprunt=(Button)findViewById(R.id.button_demande_emprunt);
+        nouveauEmprunt=(Button)findViewById(R.id.button_nouveau_emprunt);
+        nouvelleDemandeEmprunt=(Button)findViewById(R.id.button_nouveau_DemandeEmprunt);
+        button_dateDebut_emprunt=(Button)findViewById(R.id.button_dateDebut_emprunt);
+        button_heureDebut_emprunt=(Button)findViewById(R.id.button_heureDebut_emprunt);
+        button_dateFin_emprunt=(Button)findViewById(R.id.button_dateFin_emprunt);
+        button_heureFin_emprunt=(Button)findViewById(R.id.button_heureFin_emprunt);
 
         Imageproduit = (ImageView) findViewById(R.id.imageView_imageAfficherProduit_emprunt);
         star1=(ImageButton)findViewById(R.id.imageButton_rating_1);
@@ -54,8 +107,21 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
         textView_minute = (TextView)findViewById(R.id.textView_countdown_minute);
         textView_seconde = (TextView)findViewById(R.id.textView_countdown_seconde);
 
+
+
+
+        layout_boutton_emprunt= findViewById(R.id.layout_boutton_emprunt);
+        layout_saisirDate_emprunt= findViewById(R.id.layout_saisirDate_emprunt);
+        layout_demande_emprunt_boutton= findViewById(R.id.layout_demande_emprunt_boutton);
+        layout_saisirDate_DemandeEmprunt= findViewById(R.id.layout_saisirDate_DemandeEmprunt);
+        layout_demande_emprunt_countdown= findViewById(R.id.layout_demande_emprunt_countdown);
         linear_layout_jour = findViewById(R.id.LinearLayout_Jour);
         linear_layout_heure = findViewById(R.id.LinearLayout_Heure);
+
+        datePickerDebutEmprunt = new DatePickerFragment();
+        datePickerFinEmprunt = new DatePickerFragment();
+        timePickerDebutEmprunt = new TimePickerFragment();
+        timePickerFinEmprunt = new TimePickerFragment();
 
         Picasso.get().load("http://makcenter.ma/uge/projetAndroid/IMG_20200329_012624_2710855663484451951.jpg")
                 .resize(150, 150)
@@ -72,6 +138,7 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
                 star3.setImageResource(R.drawable.star0);
                 star4.setImageResource(R.drawable.star0);
                 star5.setImageResource(R.drawable.star0);
+                note =1;
 
             }
         });
@@ -84,6 +151,7 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
                 star3.setImageResource(R.drawable.star0);
                 star4.setImageResource(R.drawable.star0);
                 star5.setImageResource(R.drawable.star0);
+                note =2;
 
             }
         });
@@ -96,6 +164,7 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
                 star3.setImageResource(R.drawable.star1);
                 star4.setImageResource(R.drawable.star0);
                 star5.setImageResource(R.drawable.star0);
+                note =3;
 
             }
         });
@@ -108,6 +177,7 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
                 star3.setImageResource(R.drawable.star1);
                 star4.setImageResource(R.drawable.star1);
                 star5.setImageResource(R.drawable.star0);
+                note =4;
 
             }
         });
@@ -120,9 +190,90 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
                 star3.setImageResource(R.drawable.star1);
                 star4.setImageResource(R.drawable.star1);
                 star5.setImageResource(R.drawable.star1);
+                note =5;
 
             }
         });
+
+        layout_saisirDate_DemandeEmprunt.setVisibility(View.GONE);
+        layout_saisirDate_emprunt.setVisibility(View.GONE);
+
+        emprunter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_saisirDate_emprunt.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        demandeEmprunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_saisirDate_DemandeEmprunt.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+
+        button_dateDebut_emprunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                datePickerDebutEmprunt.show(getSupportFragmentManager(), "Datedebut");
+                button_dateDebut_emprunt.setText(date);
+                Log.e("DateDebut","-> "+date);
+            }
+        });
+
+
+        button_dateFin_emprunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                datePickerFinEmprunt.show(getSupportFragmentManager(), "Datefin");
+                button_dateFin_emprunt.setText(date);
+                Log.e("DateFin","-> "+date);
+            }
+        });
+
+        button_heureDebut_emprunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                timePickerDebutEmprunt.show(getSupportFragmentManager(), "Heuredebut");
+                button_heureDebut_emprunt.setText(heure);
+                Log.e("HeureDebut","-> "+heure);
+            }
+        });
+
+        button_heureFin_emprunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                timePickerFinEmprunt.show(getSupportFragmentManager(), "Heurefin");
+                button_heureFin_emprunt.setText(heure);
+                Log.e("HeureFin","-> "+heure);
+            }
+        });
+
+
+
+
+
+        if(product.isAvailable()){
+            layout_demande_emprunt_boutton.setVisibility(View.GONE);
+            layout_demande_emprunt_countdown.setVisibility(View.GONE);
+
+
+        }//product is available
+        else {
+            layout_boutton_emprunt.setVisibility(View.GONE);
+
+
+        }//product is not available
 
 
 
@@ -131,12 +282,6 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
     }
 
 
-
-
-
-    private void initUI() {
-
-    }
 
     private void countDownStart() {
         runnable = new Runnable() {
@@ -174,5 +319,34 @@ public class AfficherProduitEmprunt extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month++;
+        date = dayOfMonth+"/"+month+"/"+year;
+        Log.e("ksjrfs" , view.getAccessibilityClassName().toString());
+        //Log.e("tag" , "->"+view.getTag().toString());
+        Log.e("date" , "->"+date);
+
+        if(view.equals(datePickerDebutEmprunt)){
+            Log.e("dateDebut" , "->"+date);
+        }
+        else if (view.equals(datePickerFinEmprunt)){
+            Log.e("dateFin" , "->"+date);
+        }
+        else {
+            Log.e("za" , "->"+date);
+        }
+        Log.e("id" , "->"+view.getTag(1));
+        FragmentManager fm = getSupportFragmentManager();
+        DialogFragment df = (DialogFragment) fm.findFragmentByTag("Datedebut");
+       
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        heure=hourOfDay+":"+minute;
     }
 }
