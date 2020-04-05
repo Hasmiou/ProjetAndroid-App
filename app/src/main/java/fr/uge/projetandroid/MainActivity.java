5758eca5f25068008e0b7af47fe128f415d5fc21
Bahhou <mohsinebahhou@gmail.com>
2020-04-05 06:39:55 +0200

    WIP Add Menu

--- app/src/main/java/fr/uge/projetandroid/MainActivity.java
+++ app/src/main/java/fr/uge/projetandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.uge.projetandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -77,24 +78,26 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        /*
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_emprunt_accueil) {
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav__emprunt_retourner) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav__emprunt_emprunter) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav__emprunt_mesproduits) {
+            Intent myIntent = new Intent(this, AfficherCatalogueProduitsEmprunt.class);
+            startActivity(myIntent);
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav__emprunt_deconnexion) {
 
         }
-*/
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
