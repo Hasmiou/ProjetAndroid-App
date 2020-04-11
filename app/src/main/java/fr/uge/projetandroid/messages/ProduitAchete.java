bd35c15ebf8512488b01ec13da2f658c8a28fdc2
Sersouri Fatima Zohra <fati.sersouri@gmail.com>
2020-04-11 20:22:33 +0200

    WIP update message layouts

---
+++ app/src/main/java/fr/uge/projetandroid/messages/ProduitAchete.java
@@ -0,0 +1,15 @@
+package fr.uge.projetandroid.messages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.uge.projetandroid.R;
+
+public class ProduitAchete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produit_achete);
+    }
+}
